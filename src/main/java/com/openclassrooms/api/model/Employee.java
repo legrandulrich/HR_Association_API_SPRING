2c6592872dd03bb2a1a81a4cd08df533fbef8855
Rich40 <legrandulrich@gmail.com>
2022-05-16 05:44:36 +0100

    first commit

---
+++ src/main/java/com/openclassrooms/api/model/Employee.java
@@ -0,0 +1,34 @@
+package com.openclassrooms.api.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employees")  
+public class Employee {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+
+	@Column(name="mail")
+	private String mail;
+
+	@Column(name="password")
+	private String password;
+
+    
+}
